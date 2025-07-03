d138ad74a4d510d99fe2ab070e6c6912ba8d13fe
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2025-07-02 20:52:54 -0600

    progress

--- src/main/kotlin/com/jabaddon/learning/embabel/sourcer/WebPageSummaryAgent.kt
+++ src/main/kotlin/com/jabaddon/learning/embabel/sourcer/WebPageSummaryAgent.kt
@@ -1,5 +1,6 @@
 package com.jabaddon.learning.embabel.sourcer
 
+import com.embabel.agent.api.annotation.AchievesGoal
 import com.embabel.agent.api.annotation.Action
 import com.embabel.agent.api.annotation.Agent
 import com.embabel.agent.api.annotation.usingDefaultLlm
@@ -10,25 +11,40 @@ data class SearchKey(
     val query: String
 )
 
+data class WebSearchResult(
+    val title: String,
+    val link: String,
+    val snippet: String
+)
+
 @Agent(
     name = "WebPageSummaryAgent",
     description = "Agent to summarize web pages",
+
 )
 class WebPageSummaryAgent {
 
     @Action
-    fun start(input: UserInput): SearchKey {
+    fun extractSearchKey(input: UserInput): SearchKey {
         return usingDefaultLlm.createObject("""
             Extract the search query from the user input: ${input.content}
         """.trimIndent())
     }
 
     @Action(
+        toolGroups = ["web", "math"],
     )
-    fun searchWebPages(searchKey: SearchKey): String {
-        return usingDefaultLlm. createObject("""
-            Search the web for the query: ${searchKey.query}
-            Return the first result as a summary.
+    fun searchWebPages(searchKey: SearchKey): List<WebSearchResult> {
+        return usingDefaultLlm.createObject("""
+            Using web tools search the web for the query: ${searchKey.query}
+            
+            Extract the title, link, and snippet from each search results.
         """.trimIndent())
+    }
+
+    @Action
+    @AchievesGoal(description = "Summarized the web pages for the user")
+    fun summarizeWebPages(webSearchResults: List<WebSearchResult>): List<WebSearchResult> {
+        return webSearchResults
     }
 }
--- src/test/kotlin/com/jabaddon/learning/embabel/fistagent/firstagent/FirstAgentApplicationTests.kt
+++ src/test/kotlin/com/jabaddon/learning/embabel/fistagent/firstagent/FirstAgentApplicationTests.kt
@@ -1,12 +1,7 @@
 package com.jabaddon.learning.embabel.fistagent.firstagent
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
 class FirstAgentApplicationTests {
 
-	@Test
 	fun contextLoads() {
 	}
 
